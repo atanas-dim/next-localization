03b004f980ed80ae5b8bc7c359c16e3d44566684
Atanas Dimitrov <hi.atanasdim@gmail.com>
2025-06-22 21:13:14 +0100

    Reorganise app layout files

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,29 +1,9 @@
-import '@/app/globals.css'
-
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-
 import Dictionary from '@/components/Dictionary'
 import LanguagePicker from '@/components/LanguagePicker'
 import { getDictionary } from '@/dictionaries'
 import { getLocale } from '@/utils/locales'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
-
-export const metadata: Metadata = {
-  title: 'NextJS Localization',
-  description: '',
-}
-
-export default async function RootLayout({
+export default async function LocaleLayout({
   children,
   params,
 }: Readonly<{
@@ -35,14 +15,11 @@ export default async function RootLayout({
   const dict = await getDictionary(locale)
 
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} font-geist-sans flex flex-col items-center bg-white p-4 text-black antialiased`}>
-        <Dictionary dict={dict} locale={locale}>
-          <LanguagePicker />
-          {children}
-        </Dictionary>
-      </body>
-    </html>
+    <>
+      <Dictionary dict={dict} locale={locale}>
+        <LanguagePicker />
+        {children}
+      </Dictionary>
+    </>
   )
 }
---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import '@/app/globals.css'
+
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'NextJS Localization',
+  description: '',
+}
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} font-geist-sans flex flex-col items-center bg-white p-4 text-black antialiased`}>
+        {children}
+      </body>
+    </html>
+  )
+}
